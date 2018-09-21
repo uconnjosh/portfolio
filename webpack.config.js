397d2771d74c57a9374d8572af32542b5b78345b
Josh Paul <josh4044@gmail.com>
2018-09-21 00:03:24 -0700

    First commit. Setup bones of jekyll site. Added webpack preprocessing and normalize.css. Created markup/styles for the main content + menu bar.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./assets/javascripts/index.js",
+  output: {
+    path: path.resolve(__dirname, 'assets/javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  }
+};
